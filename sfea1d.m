7347bb18497bb9e21f07801fcbb97cf107848799
Sunil Anandatheertha <66170566+SunilAnandatheertha@users.noreply.github.com>
2021-01-13 13:44:56 +0000

    Removed SFEAM files
    Removed the molecular mechanics FEA files. They go to a different future repository

--- sfea1d.m
+++
@@ -1,5 +0,0 @@
-function sfea1d()
-
-[ElemDef, ElemStress, ElemStrain, NodalDISP, FinalNdPostions, FinalNdForces] = FEANL1DB();
-
-end
--- sfea2.m
+++
@@ -1,8 +0,0 @@
-function sfea2()
-
-%% SINGLE GRAPHENE SHEET STRESS-STRAIN ANANLYSIS
-[xsgs, ysgs, cov_gra, vd1_gra, vd2_gra] = SingleGrapheneSheet(10, 0, 3, 2);
-
-FEA_2d_SingleGrapheneSheet(xsgs,ysgs,cov_gra,vd1_gra,vd2_gra);
-
-end
