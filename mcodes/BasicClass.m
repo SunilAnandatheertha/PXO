327b19fcabf470905aae61d918c3ca7495e3a639
Sunil Anandatheertha <66170566+SunilAnandatheertha@users.noreply.github.com>
2021-01-15 13:49:15 +0000

    Basic informations

---
+++ mcodes/BasicClass.m
@@ -0,0 +1,8 @@
+classdef Rectangle
+    % Rectangle Data set class
+    properties
+        Width       %width of rectangle
+        Height      %height of rectangle
+        Colour      %colour of rectangle
+    end
+end
