cf2f7bd9b225e81c255a8bbab7631dbdaaf3949d
Sunil Anandatheertha <66170566+SunilAnandatheertha@users.noreply.github.com>
2021-07-28 14:29:12 +0100

    Delete unwanted m files

--- mcodes/BasicClass.m
+++
@@ -1,8 +0,0 @@
-classdef Rectangle
-    % Rectangle Data set class
-    properties
-        Width       %width of rectangle
-        Height      %height of rectangle
-        Colour      %colour of rectangle
-    end
-end
--- mcodes/ToC_testing.m
+++
@@ -1,7 +0,0 @@
-function ToC_testing()
-
-a = 0;
-for count = 1:10
-    a = a + count;
-end
-end
